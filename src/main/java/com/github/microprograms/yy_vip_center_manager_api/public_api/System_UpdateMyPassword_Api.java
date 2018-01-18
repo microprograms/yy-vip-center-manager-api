dbaf4c92115f3b4e572c70d8b307775d42cfa6f3
xuzewei <xuzewei_2012@126.com>
2018-01-18 16:49:17 +0800

    System_UpdateMyPassword_Api.java

--- src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/System_UpdateMyPassword_Api.java
+++ src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/System_UpdateMyPassword_Api.java
@@ -1,20 +1,34 @@
 package com.github.microprograms.yy_vip_center_manager_api.public_api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.microprograms.micro_api_runtime.annotation.MicroApi;
 import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
-import com.github.microprograms.micro_api_runtime.enums.MicroApiReserveResponseCodeEnum;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.utils.MicroApiUtils;
 import com.github.microprograms.micro_nested_data_model_runtime.Comment;
 import com.github.microprograms.micro_nested_data_model_runtime.Required;
+import com.github.microprograms.micro_oss_core.MicroOss;
+import com.github.microprograms.micro_oss_core.model.Field;
+import com.github.microprograms.micro_oss_core.model.dml.Condition;
+import com.github.microprograms.yy_vip_center_manager_api.utils.Fn;
 
 @MicroApi(comment = "系统 - 修改我的密码", type = "read", version = "v0.0.3")
 public class System_UpdateMyPassword_Api {
 
     private static void core(Req req, Response resp) throws Exception {
-        Object doSomeThingHere;
-        throw new MicroApiPassthroughException(MicroApiReserveResponseCodeEnum.api_not_implemented_exception);
+        AdminUser adminUser = Fn.queryAdminUserByToken(req.getToken());
+        if (adminUser == null) {
+            throw new MicroApiPassthroughException(ErrorCodeEnum.invalid_token);
+        }
+        if (!adminUser.getLoginPassword().equals(req.getOldLoginPassword())) {
+            throw new MicroApiPassthroughException(ErrorCodeEnum.invalid_password);
+        }
+        List<Field> fields = new ArrayList<>();
+        fields.add(new Field("loginPassword", req.getNewLoginPassword()));
+        MicroOss.updateObject(AdminUser.class, fields, Condition.build("token=", req.getToken()));
     }
 
     public static Response execute(Request request) throws Exception {
