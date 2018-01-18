2d6eebc5b67139ad481c773ca874be13b67f28b4
xuzewei <xuzewei_2012@126.com>
2018-01-18 20:43:25 +0800

    GoodsCategory_Update_Api.java

--- src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/GoodsCategory_Delete_Api.java
+++ src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/GoodsCategory_Delete_Api.java
@@ -1,22 +1,24 @@
 package com.github.microprograms.yy_vip_center_manager_api.public_api;
 
 import com.github.microprograms.micro_api_runtime.annotation.MicroApi;
-import com.github.microprograms.micro_api_runtime.model.Operator;
-import com.github.microprograms.micro_oss_core.model.dml.Condition;
-import com.github.microprograms.micro_oss_core.MicroOss;
-import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
 import com.github.microprograms.micro_api_runtime.enums.MicroApiReserveResponseCodeEnum;
-import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
+import com.github.microprograms.micro_api_runtime.model.Operator;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.utils.MicroApiUtils;
 import com.github.microprograms.micro_nested_data_model_runtime.Comment;
 import com.github.microprograms.micro_nested_data_model_runtime.Required;
+import com.github.microprograms.micro_oss_core.MicroOss;
+import com.github.microprograms.micro_oss_core.exception.MicroOssException;
+import com.github.microprograms.micro_oss_core.model.dml.Condition;
+import com.github.microprograms.yy_vip_center_manager_api.utils.Fn;
 
 @MicroApi(comment = "商品类别 - 删除商品类别", type = "read", version = "v0.0.4")
 public class GoodsCategory_Delete_Api {
 
-    private static Operator<?> getOperator(Req req) {
-        return null;
+    private static Operator<?> getOperator(Req req) throws MicroOssException {
+        return Fn.buildOperator(GoodsCategory_Delete_Api.class, req.getToken());
     }
 
     private static Condition buildFinalCondition(Req req) {
@@ -44,9 +46,7 @@ public class GoodsCategory_Delete_Api {
 
     public static class Req extends Request {
 
-        @Comment(value = "Token")
-        @Required(value = true)
-        private String token;
+        @Comment(value = "Token") @Required(value = true) private String token;
 
         public String getToken() {
             return token;
@@ -56,9 +56,7 @@ public class GoodsCategory_Delete_Api {
             this.token = token;
         }
 
-        @Comment(value = "商品类别ID")
-        @Required(value = true)
-        private String categoryId;
+        @Comment(value = "商品类别ID") @Required(value = true) private String categoryId;
 
         public String getCategoryId() {
             return categoryId;
