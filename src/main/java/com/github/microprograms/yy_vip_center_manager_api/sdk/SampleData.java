5818bdc9a24a8960bf6a3dc596503d3a0d73bf33
xuzewei <xuzewei_2012@126.com>
2018-01-22 22:32:54 +0800

    SampleData.java

--- src/main/java/com/github/microprograms/yy_vip_center_manager_api/sdk/SampleData.java
+++ src/main/java/com/github/microprograms/yy_vip_center_manager_api/sdk/SampleData.java
@@ -1,9 +1,14 @@
 package com.github.microprograms.yy_vip_center_manager_api.sdk;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
+import com.alibaba.fastjson.JSON;
 import com.github.microprograms.micro_oss_core.MicroOss;
+import com.github.microprograms.micro_oss_core.model.dml.Condition;
 import com.github.microprograms.yy_vip_center_manager_api.public_api.AdminUser;
+import com.github.microprograms.yy_vip_center_manager_api.public_api.Goods;
 import com.github.microprograms.yy_vip_center_manager_api.public_api.GoodsCategory;
 import com.github.microprograms.yy_vip_center_manager_api.utils.Fn;
 
@@ -13,6 +18,7 @@ public class SampleData {
         Fn.initOss();
         addAdminUsers();
         addGoodsCategories();
+        addGoods();
     }
 
     private static void addAdminUsers() throws Exception {
@@ -33,6 +39,10 @@ public class SampleData {
         MicroOss.insertObject(buildGoodsCategory(3, "分类三"));
     }
 
+    private static GoodsCategory queryGoodsCategoryByName(String name) throws Exception {
+        return MicroOss.queryObject(GoodsCategory.class, Condition.build("name=", name));
+    }
+
     private static GoodsCategory buildGoodsCategory(int reorder, String name) throws Exception {
         GoodsCategory category = new GoodsCategory();
         category.setId(UUID.randomUUID().toString());
@@ -42,5 +52,31 @@ public class SampleData {
         category.setCreaterLoginName("");
         category.setDtCreate(System.currentTimeMillis());
         return category;
+    }
+
+    private static void addGoods() throws Exception {
+        for (int i = 1; i < 50; i++) {
+            MicroOss.insertObject(buildGoods(i, "测试商品" + i, "测试商品的描述信息" + i, Arrays.asList("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1516641377045&di=dfd29501767acaf9e4ab7d372b34081d&imgtype=0&src=http%3A%2F%2Fpic31.photophoto.cn%2F20140608%2F0021033853003391_b.jpg")));
+        }
+    }
+
+    private static Goods buildGoods(int reorder, String name, String desc, List<String> pictureUrls) throws Exception {
+        GoodsCategory category = queryGoodsCategoryByName("分类一");
+        Goods goods = new Goods();
+        goods.setId(UUID.randomUUID().toString());
+        goods.setName(name);
+        goods.setDesc(desc);
+        goods.setReorder(reorder);
+        goods.setCategoryId(category.getId());
+        goods.setCategoryName(category.getName());
+        goods.setPictureUrls(JSON.toJSONString(pictureUrls));
+        goods.setPrice(99800);
+        goods.setPriceLevel1(89800);
+        goods.setPriceLevel2(79800);
+        goods.setPriceLevel3(69800);
+        goods.setCreaterId("");
+        goods.setCreaterLoginName("");
+        goods.setDtCreate(System.currentTimeMillis());
+        return goods;
     }
 }
