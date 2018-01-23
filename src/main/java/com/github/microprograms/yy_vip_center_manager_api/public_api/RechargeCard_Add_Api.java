caf029be2b7a39b783dfda1408833235ba3c084a
xuzewei <xuzewei_2012@126.com>
2018-01-23 16:09:09 +0800

    come true

--- src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/RechargeCard_Add_Api.java
+++ src/main/java/com/github/microprograms/yy_vip_center_manager_api/public_api/RechargeCard_Add_Api.java
@@ -1,16 +1,16 @@
 package com.github.microprograms.yy_vip_center_manager_api.public_api;
 
 import com.github.microprograms.micro_api_runtime.annotation.MicroApi;
-import com.github.microprograms.micro_api_runtime.model.Operator;
-import com.github.microprograms.micro_oss_core.MicroOss;
-import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
 import com.github.microprograms.micro_api_runtime.enums.MicroApiReserveResponseCodeEnum;
-import com.github.microprograms.yy_vip_center_manager_api.utils.Fn;
-import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
+import com.github.microprograms.micro_api_runtime.model.Operator;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.utils.MicroApiUtils;
 import com.github.microprograms.micro_nested_data_model_runtime.Comment;
 import com.github.microprograms.micro_nested_data_model_runtime.Required;
+import com.github.microprograms.micro_oss_core.MicroOss;
+import com.github.microprograms.yy_vip_center_manager_api.utils.Fn;
 
 @MicroApi(comment = "充值卡 - 单个添加", type = "read", version = "v0.0.6")
 public class RechargeCard_Add_Api {
@@ -19,8 +19,21 @@ public class RechargeCard_Add_Api {
         return Fn.buildOperator(RechargeCard_Add_Api.class, req.getToken());
     }
 
-    private static RechargeCard buildRechargeCard(Req req) {
+    private static RechargeCard buildRechargeCard(Req req) throws Exception {
         RechargeCard rechargeCard = new RechargeCard();
+        String rawPasswordSeriesCode = req.getRawPasswordSeriesCode();
+        int lastIndexOfSeparator = rawPasswordSeriesCode.lastIndexOf("@");
+        if (lastIndexOfSeparator == -1) {
+            throw new MicroApiPassthroughException(MicroApiReserveResponseCodeEnum.unknown_operator_exception);
+        }
+        String amountString = rawPasswordSeriesCode.substring(lastIndexOfSeparator + 1);
+        rechargeCard.setId(rawPasswordSeriesCode);
+        rechargeCard.setRawPasswordSeriesCode(rawPasswordSeriesCode);
+        rechargeCard.setAmount(Integer.valueOf(amountString));
+        AdminUser adminUser = Fn.queryAdminUserByToken(req.getToken());
+        rechargeCard.setCreaterId(adminUser.getId());
+        rechargeCard.setCreaterLoginName(adminUser.getLoginName());
+        rechargeCard.setDtCreate(System.currentTimeMillis());
         return rechargeCard;
     }
 
@@ -44,9 +57,7 @@ public class RechargeCard_Add_Api {
 
     public static class Req extends Request {
 
-        @Comment(value = "Token")
-        @Required(value = true)
-        private String token;
+        @Comment(value = "Token") @Required(value = true) private String token;
 
         public String getToken() {
             return token;
@@ -56,9 +67,7 @@ public class RechargeCard_Add_Api {
             this.token = token;
         }
 
-        @Comment(value = "原始密码序列编码串")
-        @Required(value = true)
-        private String rawPasswordSeriesCode;
+        @Comment(value = "原始密码序列编码串") @Required(value = true) private String rawPasswordSeriesCode;
 
         public String getRawPasswordSeriesCode() {
             return rawPasswordSeriesCode;
